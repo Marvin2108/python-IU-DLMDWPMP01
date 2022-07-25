3f99e7f231475fdc93053e0083bab1ae969659ba
Marvin2108 <78601186+Marvin2108@users.noreply.github.com>
2022-07-25 21:11:10 +0200

    some little changes

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ Created on Mon Jul 11 15:10:53 2022
 # -*- coding: utf-8 -*-
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
-import sqlite3
 import sqlalchemy as db
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database
 import math
 
@@ -22,36 +19,96 @@ loading given csv files into a pandas dataframe
 
 training = pd.read_csv(
     "/Users/marvinschmitt/Library/CloudStorage/OneDrive-Persönlich/M.Sc. Data Science/06 Python/Datensatz/training.csv")
-training.set_index('x',inplace=True)
+training.set_index('x',inplace=True) # set index to x
 
 ideal = pd.read_csv(
     "/Users/marvinschmitt/Library/CloudStorage/OneDrive-Persönlich/M.Sc. Data Science/06 Python/Datensatz/ideal.csv")
-ideal.set_index('x',inplace=True)
+ideal.set_index('x',inplace=True) # set index to x
 
 test = pd.read_csv(
     "/Users/marvinschmitt/Library/CloudStorage/OneDrive-Persönlich/M.Sc. Data Science/06 Python/Datensatz/test.csv")
 
 
 """
-This section creates a database, builds up a session to keep data and
-creates new tables for training and test data
+This section creates a database, and
+creates new tables for given training and ideal datasets
 """
 
 engine = db.create_engine("sqlite:///database.sqlite")
 if not database_exists(engine.url):
     create_database(engine.url)
 
-"""#print(database_exists(engine.url), engine.url)
-
-Session = sessionmaker(bind=engine)
-session = Session()"""
-
 
 training.to_sql('training',engine, index=True, if_exists='replace') # creates new table in db out of dataframe
 ideal.to_sql('ideal',engine, index=True, if_exists='replace') # creates new table in db out of dataframe
 
 
-def visualize():
+"""
+creating own Exception class called RangeError
+"""
+class RangeError(Exception):
+# einen Konstruktor definieren
+    
+    def __init__(self):
+        
+        # eine eigene Nachricht als Attribut definieren
+        my_message = 'Eigener Fehler'
+        self.my_message = my_message
+        
+        
+"""
+This section creates Calculation super class with trainingNumber as variable
+The LeastSquareMethod inherites trainingNumber and calculates the values
+"""
+class Calculation(): 
+    
+    def __init__(self,trainingNumber):
+        self.trainingNumber = trainingNumber
+        
+class LeastSquareMethod(Calculation):
+
+    def calculate_least_square(trainingNumber):
+    #    print("o",getattr(idealY1, trainingNumber))
+
+        """
+        this function calculates for each given trainingNumber its ideal function
+        from the ideal dataset using Minimum SumLeastSquared
+
+        Output:
+        -------
+        idealFunction : TYPE
+        returns corresponding ideal function for given trainingNumber
+        e.g. y36
+        """
+        
+        # Exception for only 4 allowed trainingNumbers
+        try:
+            if trainingNumber not in ['y1','y2','y3','y4']:
+                raise RangeError
+        except:
+            print("RangeError: This is a range error. Watch out!")
+        else:
+        
+            lowestValue = 999999999 # needed for calculating lowest value 
+              
+            for column in ideal.columns: # durchlauf für jedes y aus ideal
+                 sumSquared = []  # save values of residuals per column
+                 
+                 for i in training.index: # jede Zeile im training-Datensatz
+                    diff = (training[trainingNumber][i] - ideal[column][i]) ** 2
+                    sumSquared.append(diff) # um später summenwert pro spalte aus ideal zu haben
+    
+                 newValue = sum(sumSquared)
+    
+                 if newValue < lowestValue:  # prüfen, ob ein y Wert aus ideal besser ist als der bisherige beste Wert
+                     lowestValue = newValue
+                     idealFunction = column
+            print("ideal function for",trainingNumber, "=", idealFunction)
+                
+            return idealFunction
+                        
+
+def visualize_train_to_ideal():
     """
     This function creates subplots to compare training data 
     with its corresponding ideal function
@@ -79,60 +136,6 @@ def visualize():
     #plt.scatter(training["x"], training["y4"])
     plt.show()
 
-
-class Calculation(): 
-    
-    def __init__(self,trainingNumber):
-        self.trainingNumber = trainingNumber
-        
-class Least_square_method(Calculation):
-
-    def calculate_least_square(trainingNumber):
-    #    print("o",getattr(idealY1, trainingNumber))
-
-        """
-        Parameters
-        ----------
-        trainingNumber : TYPE
-        this function calculates for each given trainingNumber its ideal function
-        from the ideal dataset using Minimum SumLeastSquared
-
-        Output:
-        -------
-        idealFunction : TYPE
-        returns corresponding ideal function for given trainingNumber
-        e.g. y36
-        """
-        
-        # Exception for only 4 allowed trainingNumbers
-        try:
-            if trainingNumber not in ['y1','y2','y3','y4']:
-                raise RangeError
-        
-        except RangeError:
-            print("This TrainingNumber", trainingNumber,  "is not in training-dataset!\n",
-                  "Please choose a value between y1 and y4")
-            
-        else:
-        
-            lowestValue = 999999999 # needed for calculating lowest value 
-              
-            for column in ideal.columns: # durchlauf für jedes y aus ideal
-                 sumSquared = []  # save values of residuals per column
-                 
-                 for i in training.index: # jede Zeile im training-Datensatz
-                    diff = (training[trainingNumber][i] - ideal[column][i]) ** 2
-                    sumSquared.append(diff) # um später summenwert pro spalte aus ideal zu haben
-    
-                 newValue = sum(sumSquared)
-    
-                 if newValue < lowestValue:  # prüfen, ob ein y Wert aus ideal besser ist als der bisherige beste Wert
-                     lowestValue = newValue
-                     idealFunction = column
-            print("ideal function for",trainingNumber, "=", idealFunction)
-                
-            return idealFunction
-                        
 
 
 def test_function():
@@ -162,19 +165,7 @@ def test_function():
    # training.plot(ax=ax,use_index=True, label='training')
     ideal_functions.plot(ax=ax)    
 
-    return test_matrix
-
-
-
-# eine eigene Exception-Klasse definieren
-class RangeError(Exception):
-# einen Konstruktor definieren
-    
-    def __init__(self):
-        
-        # eine eigene Nachricht als Attribut definieren
-        my_message = 'Eigener Fehler'
-        self.my_message = my_message            
+    return test_matrix          
 
 
 
@@ -183,16 +174,13 @@ def main():
     This is the main method of the Python script which orchestrates the program
     """
     
-    visualize()
-
-    # creating instances for each y in training dataset
-    idealY1 = Least_square_method # Instanz 1
-    idealY2 = Least_square_method # Instanz 2
-    idealY3 = Least_square_method # Instanz 3
-    idealY4 = Least_square_method # Instanz 4
-
-    
     global ideal_functions # new DF in order to save determined ideal functions
+    
+    # create instances for each training column
+    idealY1 = LeastSquareMethod # Instanz 1
+    idealY2 = LeastSquareMethod # Instanz 2
+    idealY3 = LeastSquareMethod # Instanz 3
+    idealY4 = LeastSquareMethod # Instanz 4
     
     try:
         data = {idealY1.calculate_least_square('y1') : ideal[idealY1.calculate_least_square('y1')],
@@ -203,13 +191,19 @@ def main():
                 }
     
     except KeyError:
-        print("This y-value is not within the training dataset!")
+        print("KeyError: This y-value is not within the training dataset!")
     
     else:
         ideal_functions = pd.DataFrame(data)
         print (test_function()) # returns dataframe of test_matrix
-        
+
     
+
+    visualize_train_to_ideal() # call function to visualize ideal data compared to training data
+
+
+
+
    
 """ Ausführen der Main Methode"""
 if __name__ == '__main__':
